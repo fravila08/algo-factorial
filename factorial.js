44a95f977088277d4b68196af9c4b80c786a7e71
fravila08 <fr4v1l4@gmail.com>
2025-10-06 11:43:22 -0700

    added sudo code

--- factorial.js
+++ factorial.js
@@ -1,4 +1,7 @@
 function factorial(num) {
+  // iterate through a four loop the number of times specified by num
+  // store the multiplied num times the current value
+  // return the answer
   return 0;
 }
 
