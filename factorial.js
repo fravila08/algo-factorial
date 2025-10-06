c46570f3129cdbf4fd0b71ab1b349cf1879dba9c
Student <student@example.com>
2025-10-06 18:50:12 +0000

    assignment completed

--- factorial.js
+++ factorial.js
@@ -1,8 +1,15 @@
 function factorial(num) {
+  if (num === 0){
+    return 1
+  };
+  let answer = num;
   // iterate through a four loop the number of times specified by num
-  // store the multiplied num times the current value
+  for (let i = num-1 ; i > 0; i--){
+    // store the multiplied num times the current value
+    answer = answer * i;
+  };
   // return the answer
-  return 0;
-}
+  return answer;
+};
 
 module.exports = factorial;
